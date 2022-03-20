357f3987df626485b42c3b0880a584928594c01d
siddh <siddh@DESKTOP-0CCSLP1>
2022-03-20 14:21:15 +0530

    Add project files.

---
+++ GuniApp.Web/Migrations/20220320070647_Students.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GuniApp.Web.Migrations
+{
+    public partial class Students : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupDetails",
+                table: "Students");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FacultyName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FacultyName",
+                table: "Students");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GroupDetails",
+                table: "Students",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
